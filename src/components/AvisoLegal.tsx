e15909da449be8a07c09279ce3399d23b724a365
cosmemarins <cosme.marins@gmail.com>
2024-09-09 15:33:52 -0300

    inclusão do texto aviso legal

---
+++ src/components/AvisoLegal.tsx
@@ -0,0 +1,30 @@
+import { Typography } from '@mui/material'
+
+const AvisoLegal = () => {
+  return (
+    <div className='flex flex-col gap-y-1'>
+      <Typography align='justify' color='text.primary' gutterBottom={true}>
+        <b>Finalidade desse formulário:</b> As informações aqui inseridas pelo solicitante serão utilizadas
+        exclusivamente para conferência e encaminhamento ao ICABANK com a finalidade de apresentar o interesse na
+        contratação do Investimento ICAINVEST na modalidade P2P. As regras e condições serão estabelecidas através de
+        contrato enviado posteriormente pelo ICABANK ao solicitante.
+      </Typography>
+      <Typography align='justify' color='text.primary' gutterBottom={true}>
+        Essa proposta será analisada através de procedimentos previstos nos manuais de PLDFT (Políticas de Prevenção
+        contra Lavagem de Dinheiro e Financiamento ao Terrorismo) da Smart Money Group Consultoria. Os dados aqui
+        preenchidos devem ser verídicos e se necessário, comprovados através de documentações que poderão ser
+        solicitadas avulsamente durante o processo de compliance da proposta.
+      </Typography>
+      <Typography align='justify' color='text.primary' gutterBottom={true}>
+        Os manuais de políticas da RAGx podem ser encontrados no site www.smartmoneygroup.com.br Dúvidas podem ser
+        esclarecidas através do email: adm@smartmoneygroup.com.br.
+      </Typography>
+      <Typography align='justify' color='text.primary' gutterBottom={true}>
+        O ICABANK tem suas próprias políticas, podendo solicitar informações ou documentações que excedam às solicitadas
+        pela Smart Money Group Consultoria.
+      </Typography>
+    </div>
+  )
+}
+
+export default AvisoLegal
--- src/components/layout/vertical/FooterContent.tsx
+++ src/components/layout/vertical/FooterContent.tsx
@@ -1,54 +1,84 @@
 'use client'
 
 // Next Imports
+import { useState } from 'react'
+
 import Link from 'next/link'
 
 // Third-party Imports
 import classnames from 'classnames'
 
 // Hook Imports
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material'
+
 import useVerticalNav from '@menu/hooks/useVerticalNav'
 import useHorizontalNav from '@menu/hooks/useHorizontalNav'
 import { useSettings } from '@core/hooks/useSettings'
 
 // Util Imports
 import { verticalLayoutClasses } from '@layouts/utils/layoutClasses'
+import AvisoLegal from '@/components/AvisoLegal'
 
 const FooterContent = () => {
   // Hooks
   const { settings } = useSettings()
   const { isBreakpointReached: isVerticalBreakpointReached } = useVerticalNav()
   const { isBreakpointReached: isHorizontalBreakpointReached } = useHorizontalNav()
+  const [openDlgAviso, setOpenDlgAviso] = useState<boolean>(false)
 
   // Vars
   const isBreakpointReached =
     settings.layout === 'vertical' ? isVerticalBreakpointReached : isHorizontalBreakpointReached
 
   return (
-    <div
-      className={classnames(verticalLayoutClasses.footerContent, 'flex items-center justify-between flex-wrap gap-4')}
-    >
-      <p>
-        <span className='text-textSecondary'>{`© ${new Date().getFullYear()}, by `}</span>
-        <Link href='#' target='_blank' className='text-primary'>
-          Smart Money
-        </Link>
-        <span className='text-textSecondary'>{`  - todos os direitos reservados `}</span>
-      </p>
-      {!isBreakpointReached && (
-        <div className='flex items-center gap-4'>
+    <>
+      <div
+        className={classnames(verticalLayoutClasses.footerContent, 'flex items-center justify-between flex-wrap gap-4')}
+      >
+        <p>
+          <span className='text-textSecondary'>{`© ${new Date().getFullYear()}, by `}</span>
           <Link href='#' target='_blank' className='text-primary'>
-            Termos de uso
+            Smart Money
           </Link>
-          <Link href='#' target='_blank' className='text-primary'>
-            Documentação
-          </Link>
-          <Link href='#' target='_blank' className='text-primary'>
-            Suporte
-          </Link>
-        </div>
-      )}
-    </div>
+          <span className='text-textSecondary'>{`  - todos os direitos reservados `}</span>
+        </p>
+        {!isBreakpointReached && (
+          <div className='flex items-center gap-4'>
+            <Button
+              variant='text'
+              onClick={() => {
+                setOpenDlgAviso(true)
+              }}
+            >
+              Termos de uso
+            </Button>
+            <Link href='#' target='_blank' className='text-primary'>
+              Documentação
+            </Link>
+            <Link href='#' target='_blank' className='text-primary'>
+              Suporte
+            </Link>
+          </div>
+        )}
+      </div>
+      <Dialog maxWidth='sm' open={openDlgAviso} aria-labelledby='form-dialog-title' disableEscapeKeyDown>
+        <DialogTitle id='form-dialog-title'>Avisos Legais - LGPD - PLDFT</DialogTitle>
+        <DialogContent>
+          <AvisoLegal />
+        </DialogContent>
+        <DialogActions className='dialog-actions-dense'>
+          <Button
+            variant='contained'
+            color='primary'
+            onClick={() => {
+              setOpenDlgAviso(false)
+            }}
+          >
+            Fechar
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   )
 }
 
